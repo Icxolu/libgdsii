ae43638d8d72a24225a91ca98247b73caeee745f
Icxolu <lito_xxs@yahoo.de>
2020-04-21 12:57:11 +0200

    added setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'libgdsii',
+        version = '1.0',
+        packages = find_packages(),
+        python_requires = "~=3.8",
+        url = '',
+        license = '',
+        author = '',
+        author_email = '',
+        description = 'Library for parsing gds layout files'
+)
